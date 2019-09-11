a97a6a8e333d20389654ca04240226a1825927e6
Naïm Favier <fnaim42@gmail.com>
2019-09-11 12:22:32 +0200

    Use infix constructors for Term

--- src/Proof.hs
+++ src/Proof.hs
@@ -18,23 +18,23 @@ ps |- g = liftMaybe (lookup g ps) <|> asum (breakupPremise <$> ps) <|> breakupGo
         t1 :\/: t2 ->
             Uneither v x1 x2 <$> ((t1, Var x1):ps' |- g) <*> ((t2, Var x2):ps' |- g)
         t1 :->: t2 | Just v' <- lookup t1 ps ->
-            (t2, Apply v v'):ps' |- g
+            (t2, v :$: v'):ps' |- g
         t11 :/\: t12 :->: t2 ->
-            (t11 :->: t12 :->: t2, Lambda x1 (Lambda x2 (Apply v (Pair (Var x1) (Var x2))))):ps' |- g
+            (t11 :->: t12 :->: t2, x1 :\->: x2 :\->: v :$: (Var x1 :*: Var x2)):ps' |- g
         t11 :\/: t12 :->: t2 ->
             pl:pr:ps' |- g where
-                pl = (t11 :->: t2, Lambda x1 (Apply v (applyLeft (Var x1))))
-                pr = (t12 :->: t2, Lambda x2 (Apply v (applyRight (Var x2))))
+                pl = (t11 :->: t2, x1 :\->: v :$: (Var "Left" :$: Var x1))
+                pr = (t12 :->: t2, x2 :\->: v :$: (Var "Right" :$: Var x2))
         t1@(t11 :->: t12) :->: t2 -> do
-            r <- (t12 :->: t2, Lambda x1 (Apply v (Lambda "_" (Var x1)))):ps' |- t1
-            (t2, Apply v r):ps' |- g
+            r <- (t12 :->: t2, x1 :\->: v :$: ("_" :\->: Var x1)):ps' |- t1
+            (t2, v :$: r):ps' |- g
         _ -> empty
         where ps' = delete p ps
 
     breakupGoal t = case t of
-        t1 :/\: t2 -> Pair <$> (ps |- t1) <*> (ps |- t2)
-        t1 :\/: t2 -> applyLeft <$> (ps |- t1) <|> applyRight <$> (ps |- t2)
-        t1 :->: t2 -> Lambda x1 <$> ((t1, Var x1):ps |- t2)
+        t1 :/\: t2 -> (:*:) <$> (ps |- t1) <*> (ps |- t2)
+        t1 :\/: t2 -> (Var "Left" :$:) <$> (ps |- t1) <|> (Var "Right" :$:) <$> (ps |- t2)
+        t1 :->: t2 -> (x1 :\->:) <$> ((t1, Var x1):ps |- t2)
         _ -> empty
 
     x1:x2:_ = unusedSymbols ps
--- src/Term.hs
+++ src/Term.hs
@@ -2,39 +2,39 @@ module Term where
 
 import Control.Monad
 
+infixl 3 :*:
+infixl 9 :$:
+infixr 0 :\->:
 data Term = Var String
-          | Pair Term Term
+          | Term :*: Term
+          | Term :$: Term
+          | String :\->: Term
           | Unpair Term String String Term
           | Uneither Term String String Term Term
-          | Apply Term Term
-          | Lambda String Term
           deriving (Read, Eq)
 
 instance Show Term where
     showsPrec d (Var x) = showString x
-    showsPrec d (Pair l r) =
+    showsPrec d (l :*: r) =
         showChar '(' . showsPrec 0 l . showString ", " . showsPrec 0 r . showChar ')'
+    showsPrec d (v :$: v') = showParen (d > prec) $
+        showsPrec prec v . showChar ' ' . showsPrec (prec + 1) v'
+        where prec = 10
+    showsPrec d (x :\->: v) = showParen (d > prec) $
+        showChar '\\' . showString x . showString " -> " . showsPrec prec v
+        where prec = 0
     showsPrec d (Unpair p x1 x2 e) = showParen (d > prec) $
         showString ("let (" ++ x1 ++ ", " ++ x2 ++ ") = ") . showsPrec prec p . showString " in " . showsPrec prec e
         where prec = 0
     showsPrec d (Uneither e x1 x2 l r) = showParen (d > prec) $
         showString "case " . showsPrec prec e . showString (" of { Left " ++ x1 ++ " -> ") . showsPrec prec l . showString ("; Right " ++ x2 ++ " -> ") . showsPrec prec r . showString " }"
         where prec = 0
-    showsPrec d (Apply v v') = showParen (d > prec) $
-        showsPrec prec v . showChar ' ' . showsPrec (prec + 1) v'
-        where prec = 10
-    showsPrec d (Lambda x v) = showParen (d > prec) $
-        showChar '\\' . showString x . showString " -> " . showsPrec prec v
-        where prec = 0
 
 symbols = [1..] >>= (`replicateM` ['a'..'z'])
 
 x `unusedIn` Var x' = x /= x'
-x `unusedIn` Pair t1 t2 = x `unusedIn` t1 && x `unusedIn` t2
-x `unusedIn` Apply t1 t2 = x `unusedIn` t1 && x `unusedIn` t2
-x `unusedIn` Lambda x' t = x /= x' && x `unusedIn` t
+x `unusedIn` (t1 :*: t2) = x `unusedIn` t1 && x `unusedIn` t2
+x `unusedIn` (t1 :$: t2) = x `unusedIn` t1 && x `unusedIn` t2
+x `unusedIn` (x' :\->: t) = x /= x' && x `unusedIn` t
 
 unusedSymbols ps = [x | x <- symbols, all ((x `unusedIn`) <$> snd) ps]
-
-applyLeft  = Apply (Var "Left")
-applyRight = Apply (Var "Right")

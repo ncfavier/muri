9447685ba33a62f83af142f6d7ee3c45f808f109
Naïm Favier <fnaim42@gmail.com>
2019-09-11 04:06:35 +0200

    Naming

--- src/Proof.hs
+++ src/Proof.hs
@@ -12,31 +12,31 @@ liftMaybe = maybe empty pure
 
 infix 4 |-
 ps |- g = liftMaybe (lookup g ps) <|> asum (breakupPremise <$> ps) <|> breakupGoal g where
-    breakupPremise p@(t, n) = case t of
+    breakupPremise p@(t, v) = case t of
         And t1 t2 ->
-            Unpair n v1 v2 <$> ((t1, Var v1):(t2, Var v2):ps' |- g)
+            Unpair v x1 x2 <$> ((t1, Var x1):(t2, Var x2):ps' |- g)
         Or t1 t2 ->
-            Uneither n v1 v2 <$> ((t1, Var v1):ps' |- g) <*> ((t2, Var v2):ps' |- g)
-        Function t1 t2 | Just n' <- lookup t1 ps ->
-            (t2, Apply n n'):ps' |- g
+            Uneither v x1 x2 <$> ((t1, Var x1):ps' |- g) <*> ((t2, Var x2):ps' |- g)
+        Function t1 t2 | Just v' <- lookup t1 ps ->
+            (t2, Apply v v'):ps' |- g
         Function (And t11 t12) t2 ->
-            (Function t11 (Function t12 t2), Lambda v1 (Lambda v2 (Apply n (Pair (Var v1) (Var v2))))):ps' |- g
+            (Function t11 (Function t12 t2), Lambda x1 (Lambda x2 (Apply v (Pair (Var x1) (Var x2))))):ps' |- g
         Function (Or t11 t12) t2 ->
             pl:pr:ps' |- g where
-                pl = (Function t11 t2, Lambda v1 (Apply n (applyLeft (Var v1))))
-                pr = (Function t12 t2, Lambda v2 (Apply n (applyRight (Var v2))))
+                pl = (Function t11 t2, Lambda x1 (Apply v (applyLeft (Var x1))))
+                pr = (Function t12 t2, Lambda x2 (Apply v (applyRight (Var x2))))
         Function t1@(Function t11 t12) t2 -> do
-            r <- (Function t12 t2, Lambda v1 (Apply n (Lambda "_" (Var v1)))):ps' |- t1
-            (t2, Apply n r):ps' |- g
+            r <- (Function t12 t2, Lambda x1 (Apply v (Lambda "_" (Var x1)))):ps' |- t1
+            (t2, Apply v r):ps' |- g
         _ -> empty
         where ps' = delete p ps
 
-    breakupGoal g = case g of
-        And g1 g2 -> Pair <$> (ps |- g1) <*> (ps |- g2)
-        Or g1 g2 -> applyLeft <$> (ps |- g1) <|> applyRight <$> (ps |- g2)
-        Function g1 g2 -> Lambda v1 <$> ((g1, Var v1):ps |- g2)
+    breakupGoal t = case t of
+        And t1 t2 -> Pair <$> (ps |- t1) <*> (ps |- t2)
+        Or t1 t2 -> applyLeft <$> (ps |- t1) <|> applyRight <$> (ps |- t2)
+        Function t1 t2 -> Lambda x1 <$> ((t1, Var x1):ps |- t2)
         _ -> empty
 
-    v1:v2:_ = unusedSymbols ps
+    x1:x2:_ = unusedSymbols ps
 
 prove = ([] |-)
--- src/Term.hs
+++ src/Term.hs
@@ -11,30 +11,30 @@ data Term = Var String
           deriving (Read, Eq)
 
 instance Show Term where
-    showsPrec d (Var v) = showString v
-    showsPrec d (Pair a b) =
-        showChar '(' . showsPrec 0 a . showString ", " . showsPrec 0 b . showChar ')'
-    showsPrec d (Unpair p v1 v2 e) = showParen (d > prec) $
-        showString ("let (" ++ v1 ++ ", " ++ v2 ++ ") = ") . showsPrec prec p . showString " in " . showsPrec prec e
+    showsPrec d (Var x) = showString x
+    showsPrec d (Pair l r) =
+        showChar '(' . showsPrec 0 l . showString ", " . showsPrec 0 r . showChar ')'
+    showsPrec d (Unpair p x1 x2 e) = showParen (d > prec) $
+        showString ("let (" ++ x1 ++ ", " ++ x2 ++ ") = ") . showsPrec prec p . showString " in " . showsPrec prec e
         where prec = 0
-    showsPrec d (Uneither e v1 v2 l r) = showParen (d > prec) $
-        showString "case " . showsPrec prec e . showString (" of { Left " ++ v1 ++ " -> ") . showsPrec prec l . showString ("; Right " ++ v2 ++ " -> ") . showsPrec prec r . showString " }"
+    showsPrec d (Uneither e x1 x2 l r) = showParen (d > prec) $
+        showString "case " . showsPrec prec e . showString (" of { Left " ++ x1 ++ " -> ") . showsPrec prec l . showString ("; Right " ++ x2 ++ " -> ") . showsPrec prec r . showString " }"
         where prec = 0
-    showsPrec d (Apply f x) = showParen (d > prec) $
-        showsPrec prec f . showChar ' ' . showsPrec (prec + 1) x
+    showsPrec d (Apply v v') = showParen (d > prec) $
+        showsPrec prec v . showChar ' ' . showsPrec (prec + 1) v'
         where prec = 10
-    showsPrec d (Lambda v x) = showParen (d > prec) $
-        showChar '\\' . showString v . showString " -> " . showsPrec prec x
+    showsPrec d (Lambda x v) = showParen (d > prec) $
+        showChar '\\' . showString x . showString " -> " . showsPrec prec v
         where prec = 0
 
 symbols = [1..] >>= (`replicateM` ['a'..'z'])
 
-unused v (Var v') = v /= v'
-unused v (Pair t1 t2) = unused v t1 && unused v t2
-unused v (Apply t1 t2) = unused v t1 && unused v t2
-unused v (Lambda v' t) = v /= v' && unused v t
+unused x (Var x') = x /= x'
+unused x (Pair t1 t2) = unused x t1 && unused x t2
+unused x (Apply t1 t2) = unused x t1 && unused x t2
+unused x (Lambda x' t) = x /= x' && unused x t
 
-unusedSymbols ps = filter (\v -> all ((unused v) . snd) ps) symbols
+unusedSymbols ps = filter (\x -> all ((unused x) . snd) ps) symbols
 
 applyLeft  = Apply (Var "Left")
 applyRight = Apply (Var "Right")

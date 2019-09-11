243c3adc2b9ba8ccca9ad46de7ab172e3d88c5dd
Naïm Favier <fnaim42@gmail.com>
2019-09-11 12:07:25 +0200

    Use infix constructors for Type

--- src/Proof.hs
+++ src/Proof.hs
@@ -13,28 +13,28 @@ liftMaybe = maybe empty pure
 infix 4 |-
 ps |- g = liftMaybe (lookup g ps) <|> asum (breakupPremise <$> ps) <|> breakupGoal g where
     breakupPremise p@(t, v) = case t of
-        And t1 t2 ->
+        t1 :/\: t2 ->
             Unpair v x1 x2 <$> ((t1, Var x1):(t2, Var x2):ps' |- g)
-        Or t1 t2 ->
+        t1 :\/: t2 ->
             Uneither v x1 x2 <$> ((t1, Var x1):ps' |- g) <*> ((t2, Var x2):ps' |- g)
-        Function t1 t2 | Just v' <- lookup t1 ps ->
+        t1 :->: t2 | Just v' <- lookup t1 ps ->
             (t2, Apply v v'):ps' |- g
-        Function (And t11 t12) t2 ->
-            (Function t11 (Function t12 t2), Lambda x1 (Lambda x2 (Apply v (Pair (Var x1) (Var x2))))):ps' |- g
-        Function (Or t11 t12) t2 ->
+        t11 :/\: t12 :->: t2 ->
+            (t11 :->: t12 :->: t2, Lambda x1 (Lambda x2 (Apply v (Pair (Var x1) (Var x2))))):ps' |- g
+        t11 :\/: t12 :->: t2 ->
             pl:pr:ps' |- g where
-                pl = (Function t11 t2, Lambda x1 (Apply v (applyLeft (Var x1))))
-                pr = (Function t12 t2, Lambda x2 (Apply v (applyRight (Var x2))))
-        Function t1@(Function t11 t12) t2 -> do
-            r <- (Function t12 t2, Lambda x1 (Apply v (Lambda "_" (Var x1)))):ps' |- t1
+                pl = (t11 :->: t2, Lambda x1 (Apply v (applyLeft (Var x1))))
+                pr = (t12 :->: t2, Lambda x2 (Apply v (applyRight (Var x2))))
+        t1@(t11 :->: t12) :->: t2 -> do
+            r <- (t12 :->: t2, Lambda x1 (Apply v (Lambda "_" (Var x1)))):ps' |- t1
             (t2, Apply v r):ps' |- g
         _ -> empty
         where ps' = delete p ps
 
     breakupGoal t = case t of
-        And t1 t2 -> Pair <$> (ps |- t1) <*> (ps |- t2)
-        Or t1 t2 -> applyLeft <$> (ps |- t1) <|> applyRight <$> (ps |- t2)
-        Function t1 t2 -> Lambda x1 <$> ((t1, Var x1):ps |- t2)
+        t1 :/\: t2 -> Pair <$> (ps |- t1) <*> (ps |- t2)
+        t1 :\/: t2 -> applyLeft <$> (ps |- t1) <|> applyRight <$> (ps |- t2)
+        t1 :->: t2 -> Lambda x1 <$> ((t1, Var x1):ps |- t2)
         _ -> empty
 
     x1:x2:_ = unusedSymbols ps
--- src/Type.hs
+++ src/Type.hs
@@ -4,10 +4,13 @@ import Control.Monad
 import Data.Char
 import Text.Read
 
-data Type = Type String
-          | And Type Type
-          | Or Type Type
-          | Function Type Type
+infixl 3 :/\:
+infixl 2 :\/:
+infixr 0 :->:
+data Type = TVar String
+          | Type :/\: Type
+          | Type :\/: Type
+          | Type :->: Type
           deriving (Show, Eq)
 
 instance Read Type where
@@ -15,22 +18,22 @@ instance Read Type where
         readType = do
             Ident t <- lexP
             guard $ isLower (head t)
-            return (Type t)
+            return (TVar t)
         readAnd = do
             Punc "(" <- lexP
             a        <- reset readPrec
             Punc "," <- lexP
             b        <- reset readPrec
             Punc ")" <- lexP
-            return (And a b)
+            return (a :/\: b)
         readOr = prec 10 $ do
             Ident "Either" <- lexP
             a              <- step readPrec
             b              <- step readPrec
-            return (Or a b)
+            return (a :\/: b)
         readFunction = prec 5 $ do
             a         <- step readPrec
             Punc "->" <- lexP
             b         <- readPrec
-            return (Function a b)
+            return (a :->: b)
     readListPrec = readListPrecDefault
